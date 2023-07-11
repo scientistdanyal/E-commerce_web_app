df1cf95a3bc1ad31c7215a782477b38735c9dab5
yalgar0 <dk.yalgar@gmail.com>
2023-07-11 22:45:45 +0500

    admin and models are added

--- E_commerce_web_app/shop/urls.py
+++ E_commerce_web_app/shop/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     
-    path('',views.index,name='ShopHome')
+    path('',views.index,name='ShopHome'),
+    path('about/',views.about,name='AboutUs'),
+    path('contact/',views.contact,name='ContactUs'),
+    path('traker/',views.traker,name='TrakingStatus'),
+    path('search/',views.search,name='Search'),
+    path('productview/',views.productView,name='Search'),
+    path('checkout/',views.index,name='Checkout')
 ]
--- E_commerce_web_app/shop/views.py
+++ E_commerce_web_app/shop/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse('this is about')
+
+def contact(request):
+    return HttpResponse('this is contact')
+
+def traker(request):
+    return HttpResponse('this is traker')
+
+def search(request):
+    return HttpResponse('this is search')
+
+
+
+def productView(reques):
+    return HttpResponse('this is product view')
+
+def checkout(request):
+    return HttpResponse('this is checkout')
