c08207b4452b7926438269393cef909818844b50
yalgar0 <dk.yalgar@gmail.com>
2023-07-11 06:01:08 -0400

    commit

--- E_commerce_web_app/blog/views.py
+++ E_commerce_web_app/blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("blog home page")
+    return render(request,'blog/index.html')
 
 # Create your views here.
--- E_commerce_web_app/shop/views.py
+++ E_commerce_web_app/shop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Shop Home')
+    return render(request,'shop/index.html')
